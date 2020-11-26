355b801603593f599269d46757d2115d113f6517
Vito Caputo <vcaputo@pengaru.com>
2020-11-25 19:21:37 -0800

    *: explicitly include assert.h

--- src/machid.c
+++ src/machid.c
@@ -14,6 +14,7 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <assert.h>
 #include <fcntl.h>
 #include <liburing.h>
 #include <string.h>
--- src/reclaim-tail-waste.c
+++ src/reclaim-tail-waste.c
@@ -14,6 +14,7 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <assert.h>
 #include <errno.h>
 #include <inttypes.h>
 #include <stdio.h>
