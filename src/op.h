891491fe7be7ad4568118f29e3159b9dfad9d933
Vito Caputo <vcaputo@pengaru.com>
2021-08-17 01:38:21 -0700

    op: use thunk_dispatch() for cb
    Using the thunk->dispatch() method directly as the iou_op cb leaks
    the thunk

--- src/op.h
+++ src/op.h
@@ -7,7 +7,7 @@
 /* ergonomic helper for submitting a thunk_t as a cb+cb_data pair to iou_op_queue() */
 static inline void op_queue(iou_t *iou, iou_op_t *op, thunk_t *thunk)
 {
-	return iou_op_queue(iou, op, (int (*)(void *))thunk->dispatch, thunk);
+	return iou_op_queue(iou, op, (int (*)(void *))thunk_dispatch, thunk);
 }
 
 #endif
